1d84f8ab3df13c77c369beb715a639cfbddaeaba
Alexander Davydov <dabbidob.ajiekc@gmail.com>
2023-04-06 15:23:32 +0700

    настройка отображения моделей в админке

--- mis/query/admin.py
+++ mis/query/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
+
+class ParamsInline(admin.StackedInline):
+    model = models.Params
+    extra = 0
 # Register your models here.
+@admin.register(models.Query)
+class QueryModelAdmin(admin.ModelAdmin):
+    fields = ('name', 'query', 'description')
+    list_display = ('name', 'description')
+    inlines = [
+        ParamsInline
+    ]
+
--- mis/query/models.py
+++ mis/query/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
 class Query(models.Model):
-    name = models.CharField(max_length=100)
-    query = models.TextField()
-    description = models.CharField(max_length=200)
+    name = models.CharField(max_length=100, help_text='Введите имя запроса')
+    query = models.TextField(help_text='Тело запроса')
+    description = models.CharField(max_length=200, help_text='Краткое описание')
+
+    def __str__(self):
+        return self.description
+
 
 class Params(models.Model):
+
+    class Types(models.IntegerChoices):
+        STR = 1
+        INT = 2
+        DATE = 3
+
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=20)
+    type = models.IntegerField(choices=Types.choices)
     description = models.CharField(max_length=200)
     query = models.ForeignKey(Query, on_delete=models.CASCADE)
