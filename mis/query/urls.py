aec38181cd6b7bb4a839bfe5ec2b2fa669a26d7b
Alexander Davydov <dabbidob.ajiekc@gmail.com>
2023-04-06 17:20:12 +0700

    создание страниц отображения запросов и отдельного запроса(В процессе)

--- mis/query/urls.py
+++ mis/query/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import queries,query
 app_name = 'query'
 urlpatterns = [
-
+    path('', queries),
+    path('<int:pk>',query, name='query')
 ]
--- mis/query/views.py
+++ mis/query/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Query, Params, Fields
 
 # Create your views here.
+
+def queries(request):
+    queries = Query.objects.all()
+    return render(request,'queries.html',context={'queries':queries})
+
+
+def query(request, pk):
+    query = Query.objects.get(pk=pk)
+    query_params = Params.objects.get(query=pk)
+    query_fields = Fields.objects.get(query=pk).order_by('order')
+
+    return HttpResponse(query_params.description)
